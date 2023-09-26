b1eb6d04f928200ca841b9335c2f0dac03b12edd
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-09-26 20:47:19 +0000

    skdwjxjsw

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cout << "hello world";
+
+
+int main() 
+{
+    std::cout << "age: \n";
+
+    int age;
+    std::cin >> age;
+
+    std::cout << std::endl;
+    std::cout << age;
   return 0;
 }
